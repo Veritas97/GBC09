a89207dffff6edff0a4bf9586ac9f580a54624c1
Veritas97 <lykovcl@gmail.com>
2022-12-16 12:10:34 +0900

    Решили задачу 67

---
+++ Task67/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите натуральное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int SumDig(int num)
+{
+    if (num != 0)
+    {
+        return num % 10 + SumDig(num / 10);
+    }
+    return 0;
+}
+
+Console.WriteLine($"{SumDig(numN)}");
