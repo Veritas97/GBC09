59745365cba0d61cad80b8dc0e0e5aa76695360c
Veritas97 <lykovcl@gmail.com>
2022-12-16 11:45:49 +0900

    Решили задачу 66

---
+++ Задача№66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите натуральное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+int SumNatural(int num1, int num2)
+{
+    if (num1 == num2) return num2;
+    return num2 + SumNatural(num1, num2 -1);
+}
+
+Console.Write($"{SumNatural(numN,numM)}");
