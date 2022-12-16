433f8fc5c09649415c5ceaaae5aa472ed16f60e5
Veritas97 <lykovcl@gmail.com>
2022-12-16 11:33:57 +0900

    Решили задачу 65

---
+++ Task65/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите натуральное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNum(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        Console.Write($"{num2} ");
+        NaturalNum(num1, num2 + 1);
+    }
+    
+    if (num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNum(num1 + 1, num2);
+    }
+    
+    if (num1 == num2)
+    {
+        Console.Write($"{num2} ");
+    }
+}
+
+NaturalNum(numN, numM);
