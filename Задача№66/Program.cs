fc037c2fbe4914c573c60e08956f0a3ef3d6c269
Veritas97 <lykovcl@gmail.com>
2022-12-16 11:49:35 +0900

    Решили задачу 68

--- Задача№66/Program.cs
+++ Задача№66/Program.cs
@@ -6,7 +6,7 @@ int numM = Convert.ToInt32(Console.ReadLine());
 int SumNatural(int num1, int num2)
 {
     if (num1 == num2) return num2;
-    return num2 + SumNatural(num1, num2 -1);
+    return num2 + SumNatural(num1, num2 - 1);
 }
 
-Console.Write($"{SumNatural(numN,numM)}");
+Console.Write($"{SumNatural(numN, numM)}");
---
+++ Задача№68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите натуральное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int num1, int num2)
+{
+    if (num1 == 0)
+        return num2 + 1;
+    if (num1 > 0 && num2 == 0)
+        return Akkerman(num1 - 1, 1);
+    else
+        return Akkerman(num1 - 1, Akkerman(num1, num2 - 1));
+}
+
+Console.WriteLine($"{Akkerman(numN, numM)}");
