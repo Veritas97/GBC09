f8af0ccd4c240c775e483c4c4e9d274820b71b18
Veritas97 <lykovcl@gmail.com>
2022-12-16 11:07:41 +0900

    Решили задачу 63

---
+++ Task63/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите натуральное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNum(int num)
+{
+    if (num == 0) return;
+    NaturalNum(num - 1);
+    Console.Write($"{num} ");
+}
+
+NaturalNum(numN);
