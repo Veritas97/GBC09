d0f42836777afc4cfe1d58bc539af85a009f81ee
Veritas97 <lykovcl@gmail.com>
2022-12-16 12:25:13 +0900

    Решили задачу 69

---
+++ Task69/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите натуральное число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int RecSumExpo(int num1, int num2)
+{
+    if (num2 == 0) return 1;
+    return RecSumExpo(num1, num2 - 1) * num1;
+}
+
+Console.WriteLine($"{RecSumExpo(numA,numB)}");
